7a211444c02b517e3bf6f84250a31ab1b69382e9
carolina <carolina.rojas1295@gmail.com>
2023-08-23 20:31:23 -0700

    [index] change layout, add pagination

--- encyclopedia/tests.py
+++ encyclopedia/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase
+from django.core.paginator import Page
+from django.urls import reverse
+from unittest.mock import patch
 
-# Create your tests here.
+from . import util
+
+class IndexViewTest(TestCase):
+    def test_index_view_with_entries(self):
+        entries = ["Entry1", "Entry2", "Entry3", "Entry4", "Entry5", "Entry6", "Entry7", "Entry8", "Entry9", "Entry10", "Entry11"]
+
+        # Mock the 'list_entries' function to return the list of entries
+        with patch.object(util, 'list_entries', return_value=entries):
+            response = self.client.get(reverse('index'))
+
+        self.assertEqual(response.status_code, 200)  # Check if the view returns a 200 status code
+
+        # Check if the correct template is used
+        self.assertTemplateUsed(response, 'encyclopedia/index.html')
+
+        # Check if the entries are paginated correctly (assuming 10 entries per page)
+        self.assertIsInstance(response.context['entries'], Page)
+        self.assertEqual(len(response.context['entries']), 10)  # Check the number of entries on the page
+        self.assertEqual(response.context['entries'].paginator.num_pages, 2)  # Check the number of pages
+
+    def test_index_view_without_entries(self):
+        # Mock the 'list_entries' function to return an empty list
+        with patch.object(util, 'list_entries', return_value=[]):
+            response = self.client.get(reverse('index'))
+
+        self.assertEqual(response.status_code, 200)  # Check if the view returns a 200 status code
+
+        # Check if the correct template is used
+        self.assertTemplateUsed(response, 'encyclopedia/index.html')
+
+        # Check if there are no entries displayed on the page
+        self.assertEqual(len(response.context['entries']), 0)  # Check the number of entries on the page
+        self.assertEqual(response.context['entries'].paginator.num_pages, 1)  # Check the number of pages
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,14 +1,19 @@
-from django.shortcuts import render
-from django.shortcuts import redirect
+from django.core.paginator import Paginator
+from django.shortcuts import render, redirect
 from django import forms
-
-from . import util
 import markdown2
 import random as rand
 
+from . import util
+
+
 def index(request):
+    paginator = Paginator(util.list_entries(), 10)
+    page_num = request.GET.get('page')
+    page_obj = paginator.get_page(page_num)
+
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": page_obj
     })
 
 def entry(request, title):
